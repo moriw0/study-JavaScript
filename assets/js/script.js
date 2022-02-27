aed9686d74898b701102b22d8213e860bc7db6d4
shota <tontonka2@gmail.com>
2022-02-28 06:40:15 +0900

    add ramdom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-let insertHTML = '';
+const studentNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+let insertHTML = ''; // 足し算した結果を保存して置く場所を確保する
 
-for(let i = 1; i <= 18; i++){
-  insertHTML += '<div class="seat__item">' + i + '</div>';
+for(let i = studentNumbers.length; i > 0; i--){
+  const randomNum = Math.floor(Math.random() * i);
+  let tmp = studentNumbers[i - 1]; 
+  studentNumbers[i - 1] = studentNumbers[randomNum]; 
+  studentNumbers[randomNum] = tmp;
 }
+
+console.log(studentNumbers);
+
+studentNumbers.forEach(function(num){
+  insertHTML += '<div class="seat__item">' + num + '</div>';
+})
 
 document.querySelector('#seat').innerHTML = insertHTML;
